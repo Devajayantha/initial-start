d453b00aed8e6b1a5c83249aa29320c5c42cfbf2
DevaJayantha <devajayantha888@gmail.com>
2023-05-07 14:07:04 +0800

    test demo viewShowTodoList

--- src/main/java/org/devajayantha/Main.java
+++ src/main/java/org/devajayantha/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static java.util.Scanner scanner = new java.util.Scanner(System.in);
     public static void main(String[] args) {
-        testViewRemoveTodoList();
+        viewShowTodoList();
 
         System.out.println("Hello world!");
     }

96e93869fa1777c848514904423ea66f0af8a235
DevaJayantha <devajayantha888@gmail.com>
2023-05-09 23:34:24 +0800

    + example class + constructor + constructor overloading & call each constructor

---
+++ src/main/java/org/devajayantha/Person.java
@@ -0,0 +1,22 @@
+package org.devajayantha;
+
+public class Person {
+    String name;
+    String address;
+    final String country = "Indonesia";
+
+    Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    Person(String name) {
+        this(name, null);
+    }
+
+    Person() {}
+
+    void sayHello(String name) {
+        System.out.println("Hello " + name + ", my name is " + this.name);
+    }
+}

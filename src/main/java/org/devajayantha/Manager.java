995c60f55ce69b14b52df480811269c9a5c8d58b
DevaJayantha <devajayantha888@gmail.com>
2023-05-23 22:40:35 +0800

    + example interface

--- src/main/java/org/devajayantha/Manager.java
+++ src/main/java/org/devajayantha/Manager.java
@@ -1,8 +1,16 @@
 package org.devajayantha;
 
-public class Manager extends Employee{
+public class Manager extends Employee implements HasJob{
 
     Manager(String name) {
         super(name);
+    }
+
+    public void currentProject(String name) {
+        System.out.println("Current project is "+ name);
+    }
+
+    public void currentSalary(String name) {
+        System.out.println("Current salary is "+ name);
     }
 }

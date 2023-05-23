5faa642d429be05af0fe0934afa07ba366f95549
DevaJayantha <devajayantha888@gmail.com>
2023-05-23 22:45:06 +0800

    + add default method in interface

--- src/main/java/org/devajayantha/HasJob.java
+++ src/main/java/org/devajayantha/HasJob.java
@@ -4,4 +4,8 @@ public interface HasJob {
     public void currentProject(String name);
 
     public void currentSalary(String name);
+
+    default boolean isContract() {
+        return false;
+    }
 }

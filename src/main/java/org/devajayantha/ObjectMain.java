b8f841e141cbfa3bb6347201632a290fdd260649
Devajayantha <devajayantha888@gmail.com>
2023-08-24 22:46:27 +0800

    + using object class for checking class is not null

---
+++ src/main/java/org/devajayantha/ObjectMain.java
@@ -0,0 +1,49 @@
+package org.devajayantha;
+
+import java.util.Objects;
+
+public class ObjectMain {
+    public static class Data {
+        public String name;
+        public String desc;
+
+        public Data(String name, String desc) {
+            this.name = name;
+            this.desc = desc;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+
+        @Override
+        public String toString() {
+            return "Data{" +
+                    "name='" + name + '\'' +
+                    ", desc='" + desc + '\'' +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        excute(new Data("Devajayantha", "Hello world!"));
+
+        excute(null);
+    }
+
+    public static void excute(Data data) {
+        System.out.println(Objects.toString(data));
+    }
+}

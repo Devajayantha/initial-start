309fb26b218b9d1126f2122803405bdb2f4160a1
Devajayantha <devajayantha888@gmail.com>
2023-09-04 23:17:02 +0800

    + example generic multikeys class

--- src/main/java/org/devajayantha/generic/Main.java
+++ src/main/java/org/devajayantha/generic/Main.java
@@ -5,5 +5,9 @@ public class Main {
         MyData<String> data = new MyData<String>();
         data.setData("Hello");
         System.out.println(data.getData());
+
+        Pair<String, Integer> pair = new Pair<String, Integer>("Hello", 1);
+
+        System.out.println(pair.getFirst());
     }
 }
